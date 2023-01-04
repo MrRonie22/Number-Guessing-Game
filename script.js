35863805d13494239fa808ae9870beefa155f3de
Ronie Casaclang <101075980+MrRonie22@users.noreply.github.com>
2023-01-04 21:08:01 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let guesses = 0;
+let score = 0;
+
+let yourScore = document.getElementById("score");
+document.getElementById("submit").onclick = function(){
+
+    let inputNum = document.getElementById("input").value;
+    if ((inputNum > 5 || inputNum < 1))
+    {
+        window.alert("Please enter a valid answer!");    
+    }
+    else
+    {
+        const answer = Math.floor(Math.random() * 5 +1);
+        guesses++;
+        yourScore.innerHTML = "Your Score : " +score+ "/" +guesses;
+
+        if (inputNum == answer){ 
+            window.alert("Correct Answer!"); 
+            score++;
+            yourScore.innerHTML = "Your Score : " +score+ "/" +guesses;
+        }
+        else { 
+            window.alert("Incorrect! \n Correct answer is :" +answer); 
+        }
+    }
+
+    
+}
